75da22854b4033879f6586e98c30de3492c53eb2
Matt <matt.mill@valueiron.com>
2025-11-20 13:24:24 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:lts-alpine3.22 AS build
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+COPY . .
+RUN npm run build
+ 
+# Production Stage
+FROM nginx:stable-alpine AS production
+COPY --from=build /app/dist /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+let Card = () => {
+    return(<div>
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+            <li>Five</li>
+            </ul>
+            </div>
+        );
+}
+
+export default Card;
