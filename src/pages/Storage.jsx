c65fdf53fe0fbaa964ee0d86e4ba8187652cc697
Matt <matt.mill@valueiron.com>
2025-11-20 17:13:56 -0700

    feat:add pages

---
+++ src/pages/Storage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Page.css";
+
+const Storage = () => {
+    return (
+        <div className="page-container">
+            <h1 className="page-title">Storage</h1>
+            <p className="page-description">
+                Configure and manage storage resources here.
+            </p>
+            <div className="page-content">
+                <p>Content will be displayed here...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Storage;
+
